2684dad397de3642309d4d864f5fbf8213e1d49c
PeanutZ27 <phurinjarumruay@gmail.com>
2022-08-31 10:11:07 +0700

    reupload week 3

--- 31_8_64 line foolwer 3 sensor/main/main.ino
+++ 31_8_64 line foolwer 3 sensor/main/main.ino
@@ -1,3 +1,19 @@
+#include"motor.cpp"
+// Motor R
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+// Motor L 
+int enB = 11;
+int in3 = 13;
+int in4 = 12;
+
+int turn_ms = 0;
+
+Motor L_motor(in4,in3,enB);
+Motor R_motor(in1,in2,enA);
+Dual_motor Dual(L_motor,R_motor);
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +22,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void m(int l,int r){
+    Dual.m(l,r);
 }
